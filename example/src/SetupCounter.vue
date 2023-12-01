956d94ee48caca467c04989037598f3a2ed4cc75
Tina Wang <tina@tina.cafe>
2023-12-01 10:33:37 +0100

    ✨ Add vue3 script setup support

---
+++ example/src/SetupCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
+
+<script setup>
+  /**
+   * @module test/CounterSetup
+   * @vue-prop {Number} [initialCounter=1] - Counter default value
+   * @vue-slot #header This is a header slot
+   */
+  defineProps({
+    initialCounter: number,
+  })
+</script>
